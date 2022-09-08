73f8f5c6e7a2c61a469f340b62212639dd77f16f
Sabir M. Guliyev <sabirmg@code.local>
2022-09-08 13:06:32 +0400

    Task finished

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -2,31 +2,60 @@ use Northwind
 
 go
 
-ALTER FUNCTION PhoneCheck(
-    @name nvarchar(40),
-    @phone nvarchar(24)
+create FUNCTION PhoneCheck(
+    @phone as nvarchar(40)
 )
 RETURNS nvarchar(40)
 AS 
 BEGIN
-if (Len(@phone) >=12)
+   DECLARE @number AS NVARCHAR(50)
+   IF LEN(@phone) = 13
+		BEGIN
+			SET @number = CONCAT((SUBSTRING(@phone,1,3)),' ','(',(SUBSTRING(@phone,4,3)),')', ' ',(SUBSTRING(@phone,7,3)), ' ',(SUBSTRING(@phone,10,2)), ' ',(SUBSTRING(@phone,12,2)))
+		END
+	ELSE IF LEN(@phone) = 12
+		BEGIN
+		    SET @number = CONCAT('+', (SUBSTRING(@phone,1,2)),' '
+			,'(',(SUBSTRING(@phone,3,3)),')', ' ',(SUBSTRING(@phone,6,3)), ' ',(SUBSTRING(@phone,9,2)), ' ',(SUBSTRING(@phone,11,2)))
+		END
+		 IF LEN(@phone) = 11
+		BEGIN
+		    SET @number = CONCAT('+9', (SUBSTRING(@phone,1,1)),' '
+			,'(',(SUBSTRING(@phone,2,3)),')', ' ',(SUBSTRING(@phone,5,3)), ' ',(SUBSTRING(@phone,8,2)), ' ',(SUBSTRING(@phone,10,2)))
+		END
+	ELSE IF LEN(@phone) = 10
+		BEGIN
+		    SET @number = CONCAT('+90', ' (', (SUBSTRING(@phone,1,3)),')',' '
+			,(SUBSTRING(@phone,4,3)), ' ',(SUBSTRING(@phone,7,2)), ' ',(SUBSTRING(@phone,9,2)))
+		END
+	ELSE
+	BEGIN
+	  SET @number='0'
+	  END
 
-    RETURN @name;
-END;
+	 RETURN @number
+END
 
 go
 
-create Trigger ChangeFormat on Shippers
+
+create Trigger CheckPhone on Shippers
  instead of insert
  as
  begin
- DECLARE @Name AS VARCHAR(100)
  DECLARE @Number AS VARCHAR(100)
- set @Name=(select CompanyName from INSERTED)
- set @Number=(select Phone from INSERTED)
+ select @Number = Phone from inserted
+ IF dbo.Phonecheck(@Number)='0'
+	PRINT 'Number is not valid!'
+	ELSE
+	INSERT Shippers values((Select CompanyName from inserted),dbo.PhoneCheck(@Number))
+	Select 
+ end
+
+ select * from Shippers
+ insert into Shippers values('azs','9034578')
+
+
+
+
  
- EXEC  PhoneCheck @Name,@Number
-
- end;
-
- insert into Shippers values('azs','453543')
